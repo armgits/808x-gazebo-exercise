53d526243d92087f067f8f31b93bddd7c84f4d20
armgits <126507842+armgits@users.noreply.github.com>
2023-11-30 00:18:31 -0500

    Created header file for the node

---
+++ include/walker_node.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/laser_scan.hpp>
+#include <geometry_msgs/msg/twist.hpp>
