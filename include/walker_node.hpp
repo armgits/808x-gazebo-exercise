82ed8628d7e004402cf726033f86af84badce9d8
armgits <126507842+armgits@users.noreply.github.com>
2023-11-30 00:26:28 -0500

    Declared node class members

--- include/walker_node.hpp
+++ include/walker_node.hpp
@@ -8,7 +8,20 @@
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/laser_scan.hpp>
 #include <geometry_msgs/msg/twist.hpp>
+
 class Walker : public rclcpp::Node {
  public:
+  Walker();
  private:
+  void timerCallback();
+  void scannerCallback(const sensor_msgs::msg::LaserScan& data);
+  bool robotSenseLeft();
+  bool robotSenseRight();
+  void robotTurn(char direction);
+
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr walker_;
+  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scanner_;
+  rclcpp::TimerBase::SharedPtr timer_;
+
+  sensor_msgs::msg::LaserScan scan_;
 };
